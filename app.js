73f409308ee85a6598a659eff1f3ca82153cf742
Joseph Martinez <joseph.r.martinez@gmail.com>
2023-07-16 17:17:18 -0700

    Add auth functions

--- app.js
+++ app.js
@@ -27,10 +27,64 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+    new LocalStrategy(async(username, password, done) => {
+      try {
+        const user = await User.findOne({ username: username });
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        };
+        if (user.password !== password) {
+          return done(null, false, { message: "Incorrect password" });
+        };
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      };
+    })
+  );
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async function(id, done) {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch(err) {
+      done(err);
+    };
+  });
+
+
+
+
+
+
+
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/sign-up", (req, res) => res.render("sign_up_form"));
+
+app.post("/sign-up", async (req, res, next) => {
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+      const result = await user.save();
+      res.redirect("/");
+    } catch(err) {
+      return next(err);
+    };
+  });
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
